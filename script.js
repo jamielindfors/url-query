a8d0bd7d9ed6bd4481c757d9b662310e80adca4d
jamielindfors <35880062+jamielindfors@users.noreply.github.com>
2019-03-05 15:45:53 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ $(document).ready( function(){
 function getQueryVariable(variable)
 {
        var query = window.location.search.substring(1);
-       console.log('query');
+       console.log(query);
        var vars = query.split("&");
        for (var i=0;i<vars.length;i++) {
                var pair = vars[i].split("=");
                if(pair[0] == variable){
-                console.log('pair[1]');
+                console.log(pair[1]);
                   $('p4').html(pair[1]+"  Oh Yeah!");
                   }
        }
