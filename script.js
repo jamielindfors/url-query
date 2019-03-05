7907920c19cd3ae1ee554a891b133f00796f852c
jamielindfors <35880062+jamielindfors@users.noreply.github.com>
2019-03-05 14:48:29 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+
+$(document).ready( function(){
+    // wait for document to be loaded
+    console.log('document ready');
+   getQueryVariable(id)
+
+function getQueryVariable(variable)
+{
+       var query = window.location.search.substring(1);
+       var vars = query.split("&");
+       for (var i=0;i<vars.length;i++) {
+               var pair = vars[i].split("=");
+               if(pair[0] == variable){
+                  $('p4').html(pair[1]+"  Oh Yeah!");
+                  }
+       }
+       return(false);
+}
